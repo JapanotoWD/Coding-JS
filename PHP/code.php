2cfbb01f8dc813a293e32e5ee1afbe3d31f9d014
JapanotoWD <japamerzario@gmail.com>
2024-07-27 00:48:13 -0300

    first commit

---
+++ PHP/code.php
@@ -0,0 +1,29 @@
+<?php
+$nombre=$_POST['nombre']
+$email=$_POST['email']
+$telefono=$_POST['telefono']
+$to15=$_POST['13to15']
+$to18=$_POST['16to18']
+$plus18=$_POST['18plus']
+$boxeo=$_POST['boxeo']
+$spinning=$_POST['spinning']
+$stretching=$_POST['stretching']
+$pesas=$_POST['pesas']
+$functional=$_POST['functional']
+$zumba=$_POST['zumba']
+
+$mensaje="
+<strong>Nombre;</strong> $nombre <br /><br/>
+<strong>;E-mail</strong> $email <br /><br/>
+<strong>;telefono</strong> $telefono <br /><br/>
+<strong>;to15</strong> $to15 <br /><br/>
+<strong>;to18</strong> $to18 <br /><br/>
+<strong>;18plus</strong> $plus18 <br /><br/>
+<strong>;boxeo</strong> $boxeo <br /><br/>
+<strong>;spinning</strong> $spinning <br /><br/>
+<strong>;stretching</strong> $stretching <br /><br/>
+<strong>;pesas</strong> $pesas <br /><br/>
+<strong>functional;</strong> $functional <br /><br/>
+<strong>;zumba</strong> $zumba <br /><br/>
+";,.-
+?>
